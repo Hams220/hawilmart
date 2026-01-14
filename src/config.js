a08980c2a47034e774c57e6d1154962ba0d93544
Hams220 <111257232+Hams220@users.noreply.github.com>
2026-01-14 12:33:41 +0700

    Delete src directory

--- src/config.js
+++
@@ -1,6 +0,0 @@
-// API endpoint
-const API_URL = "https://hawilpay.my.id/API";
-const IMG_URL = "https://hawilpay.my.id/assets/";
-const createCartUrl = API_URL + "/create_keranjang.php";
-const addToCartUrl = API_URL + "/add_keranjang.php";
-const idKeranjang = "KRN123"; // ganti dengan keranjang user sebenarnya
